ee0a9339a66498ed136898d27f644c3727b396cd
fil <filipisee@hotmail.com>
2022-11-18 20:17:29 -0300

    configuring ts

---
+++ INTRO/script.js
@@ -0,0 +1,9 @@
+var firstName = "Philip";
+var newNumber = 2;
+var x = true;
+function greetings(name) {
+    console.log("Olá " + name);
+}
+greetings(firstName);
+//greetings(newNumber);
+//greetings(x);
---
+++ INTRO/script.ts
@@ -0,0 +1,11 @@
+const firstName = "Philip";
+const newNumber = 2;
+const x = true;
+
+function greetings(name: string) {
+  console.log("Olá " + name);
+}
+
+greetings(firstName);
+//greetings(newNumber);
+//greetings(x);

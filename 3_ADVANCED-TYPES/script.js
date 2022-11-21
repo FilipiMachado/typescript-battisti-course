0fce07b14c7b75fd3c6165d58c501209340f327e
fil <filipisee@hotmail.com>
2022-11-21 20:45:28 -0300

    testing array/number/any types

--- 3_ADVANCED-TYPES/script.js
+++ 3_ADVANCED-TYPES/script.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("Hello World!");
+const numArray = [1, 2, 3, 4];
+const numArray2 = [1, 2, 3, 4];
+const names = ["fil", "br"];
+const names2 = ["Fil", "Br", "Hue", "Zero"];
+numArray.push(5);
+console.log(numArray);
+// Tipo Any
+/*
+-- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
+-- Deve-se ao máximo evitar esse tipo
+-- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
+*/
--- 3_ADVANCED-TYPES/script.ts
+++ 3_ADVANCED-TYPES/script.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+const numArray: number[] = [1, 2, 3, 4];
+const numArray2: Array<number> = [1, 2, 3, 4];
+
+const names: string[] = ["fil", "br"];
+const names2: Array<string> = ["Fil", "Br", "Hue", "Zero"];
+
+numArray.push(5);
+
+console.log(numArray);
+
+// Tipo Any
+/* 
+-- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
+-- Deve-se ao máximo evitar esse tipo
+-- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
+*/

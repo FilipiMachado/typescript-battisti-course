5621950454404ff7d565618cbe964f97d1ec5995
fil <filipisee@hotmail.com>
2022-11-21 19:59:39 -0300

    generate config file

--- 2_FUNDAMENTALS/script.js
+++ 2_FUNDAMENTALS/script.js
@@ -1,2 +1,10 @@
-var newNumber = 2;
+/* let newNumber: number = 2;
+
 console.log(newNumber);
+ */
+// inference and annotation
+var ann = "Test";
+var inf = 'Test2';
+//ann = 2
+//inf = 1
+console.log(inf.length);
--- 2_FUNDAMENTALS/script.ts
+++ 2_FUNDAMENTALS/script.ts
@@ -3,3 +3,14 @@
 console.log(newNumber);
  */
 
+// inference and annotation
+
+let ann: string = "Test";
+
+let inf = 'Test2'
+
+//ann = 2
+//inf = 1
+
+//console.log(inf.length)
+

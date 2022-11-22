ab2209a8be466185e1aa6945f2fdabfc3ae518f9
fil <filipisee@hotmail.com>
2022-11-21 21:00:31 -0300

    testing f type params

--- 3_ADVANCED-TYPES/script.js
+++ 3_ADVANCED-TYPES/script.js
@@ -4,10 +4,16 @@ const numArray2 = [1, 2, 3, 4];
 const names = ["fil", "br"];
 const names2 = ["Fil", "Br", "Hue", "Zero"];
 numArray.push(5);
-console.log(numArray);
+//console.log(numArray);
 // Tipo Any
 /*
--- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
--- Deve-se ao máximo evitar esse tipo
--- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
+- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
+- Deve-se ao máximo evitar esse tipo
+- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
 */
+const array1 = [1, "test", true, [], { name: "Fil" }];
+//console.log(array1);
+// Parâmetros tipados
+function sum(a, b) {
+    console.log(a + b);
+}
--- 3_ADVANCED-TYPES/script.ts
+++ 3_ADVANCED-TYPES/script.ts
@@ -6,11 +6,21 @@ const names2: Array<string> = ["Fil", "Br", "Hue", "Zero"];
 
 numArray.push(5);
 
-console.log(numArray);
+//console.log(numArray);
 
 // Tipo Any
 /* 
--- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
--- Deve-se ao máximo evitar esse tipo
--- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
+- O tipo Any passa para o TS que qualquer tipo declarado é correspondente a variável
+- Deve-se ao máximo evitar esse tipo
+- Casos de uso: o tipo do dado realmente não importa ou arrays com dados de múltiplos tipos
 */
+
+const array1: any = [1, "test", true, [], { name: "Fil" }];
+//console.log(array1);
+
+// Parâmetros tipados
+function sum(a: number, b: number) {
+  console.log(a + b);
+}
+
+
